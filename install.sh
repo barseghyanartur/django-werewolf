8dae801cc0e5ef898d38d34123c4ad192293b55c
Artur Barseghyan <artur.barseghyan@gmail.com>
2013-09-10 23:57:55 +0200

    cleanup

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ python setup.py install
 pip install -r example/requirements.txt
 python example/example/manage.py collectstatic --noinput --traceback -v 3
 python example/example/manage.py syncdb --noinput --traceback -v 3
-python example/example/manage.py migrate --noinput --traceback -v 3
+#python example/example/manage.py migrate --noinput --traceback -v 3
