90d9cd6583a5861a89c7fc352343ffabe340af9b
Artur Barseghyan <artur.barseghyan@gmail.com>
2013-10-10 20:37:23 +0200

    fix automated installer

--- django-werewolf-example-app-install.sh
+++ django-werewolf-example-app-install.sh
@@ -4,6 +4,7 @@ tar -xvf django_werewolf_example_app.tar.gz -C django_werewolf_example_app
 cd django_werewolf_example_app/django-werewolf-stable/example/example/
 pip install Django
 pip install -r ../requirements.txt
+pip install -e git+https://github.com/barseghyanartur/django-werewolf@stable#egg=django-werewolf
 mkdir ../media/
 mkdir ../media/static/
 mkdir ../static/
@@ -12,4 +13,4 @@ cp local_settings.example local_settings.py
 ./manage.py syncdb --noinput --traceback -v 3
 ./manage.py collectstatic --noinput --traceback -v 3
 ./manage.py news_create_groups_and_test_users --traceback -v 3
-./manage.py runserver --traceback -v 3
+./manage.py runserver 0.0.0.0:8001 --traceback -v 3
